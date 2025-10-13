d79a8039649961afd07956b7c750b3e706d0b736
lwky <abdirsk04@hotmail.com>
2025-10-13 22:30:11 +0200

    Workout page updated

--- app/(tabs)/workout.tsx
+++ app/(tabs)/workout.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, Modal, StyleSheet, Text, View } from 'react-native';
 
 export default function WorkoutMain() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
   return (
     <View style={styles.body}>
       <View style={styles.header}>
         <Text style={styles.headerTitle}>WORKOUT</Text>
       </View>
+
+      <View style={styles.main1}>
+        <Button title="Start workout" color="black" onPress={() => setIsModalVisible(true)}/>
+      </View>
+
+      <Modal visible={isModalVisible}>
+        <View style={styles.main1}>
+          <Button title="Close" color="black" onPress={() => setIsModalVisible(false)}></Button>
+        </View>
+      </Modal>
 
       <View style={styles.main}>
         <Text style={styles.mainTitle}>Start a workout!</Text>
@@ -36,6 +47,13 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  main1: {
+    flex: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    paddingLeft: 15,
+    paddingTop: 50,
   },
   mainTitle: {
     fontSize: 18,
