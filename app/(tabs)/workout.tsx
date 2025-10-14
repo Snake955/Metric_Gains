ec3e1dc739b4aec3ce8342441cb5d0264d85b8be
lwky <abdirsk04@hotmail.com>
2025-10-14 08:30:52 +0200

    Update safeareaview

--- app/(tabs)/workout.tsx
+++ app/(tabs)/workout.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { Button, Modal, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function WorkoutMain() {
   const [isModalVisible, setIsModalVisible] = useState(false);
   return (
-    <View style={styles.body}>
+    <SafeAreaView style={styles.body}>
       <View style={styles.header}>
         <Text style={styles.headerTitle}>WORKOUT</Text>
       </View>
@@ -13,24 +14,29 @@ export default function WorkoutMain() {
         <Button title="Start workout" color="black" onPress={() => setIsModalVisible(true)}/>
       </View>
 
-      <Modal visible={isModalVisible}>
-        <View style={styles.main1}>
+      <Modal visible={isModalVisible} onRequestClose={() => setIsModalVisible(false)} animationType="slide" presentationStyle="pageSheet">
+        <SafeAreaView style={styles.modalBody}>
+        <View style={styles.modalMain}>
           <Button title="Close" color="black" onPress={() => setIsModalVisible(false)}></Button>
         </View>
+        </SafeAreaView>
       </Modal>
 
       <View style={styles.main}>
         <Text style={styles.mainTitle}>Start a workout!</Text>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
+
+const PADDING1 = 16;
 
 const styles = StyleSheet.create({
   body: {
     flex: 1,
     backgroundColor: '#fff',
     paddingTop: 50,
+    paddingHorizontal: PADDING1,
   },
   header: {
     alignItems: 'center',
@@ -59,5 +65,15 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: '#000',
     textAlign: 'center',
+  },
+  modalBody: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: PADDING1,
+  },
+  modalMain: {
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    marginTop: 12,
   },
 });
