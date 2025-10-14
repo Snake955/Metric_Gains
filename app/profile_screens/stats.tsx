e8b09a61fb6c4972e434a6fa004ea7612ef7329c
Erik H <erik.hanko@hotmail.com>
2025-10-14 21:02:06 +0200

    La til ting på stats siden

--- app/profile_screens/stats.tsx
+++ app/profile_screens/stats.tsx
@@ -1,9 +1,135 @@
-import { Text, View } from 'react-native';
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+import { IconSymbol } from "@/components/ui/icon-symbol";
+import React from "react";
+import { ScrollView, StyleSheet, View } from 'react-native';
+import * as Progress from "react-native-progress";
 
 export default function StatsScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Stats Screen</Text>
-    </View>
+    <ScrollView style={styles.container}>
+        <View style={styles.header}>
+            <IconSymbol name="bell.fill" size={24} color="#3f3f3fff" />
+        </View>
+        <ThemedText type="subtitle" style={styles.sectionTitle}>
+                Statistics
+        </ThemedText>
+        <View style={styles.activityRow}>
+        <ThemedView style={styles.activityCard}>
+            <Progress.Circle
+            size={100}
+            progress={6742 / 10000}
+            thickness={8}
+            color="#2D7FF9"
+            showsText={false}
+            />
+            <ThemedText type="defaultSemiBold">6742 steps</ThemedText>
+            <ThemedText type="default">10,000 goal</ThemedText>
+        </ThemedView>
+
+        <ThemedView style={styles.activityCard}>
+            <Progress.Circle
+            size={100}
+            progress={240 / 500}
+            thickness={8}
+            color="#2D7FF9"
+            showsText={false}
+            />
+            <ThemedText type="defaultSemiBold">240 kcal</ThemedText>
+            <ThemedText type="default">500 goal</ThemedText>
+        </ThemedView>
+
+        <ThemedView style={styles.activityCard}>
+            <ThemedText type="defaultSemiBold">Workouts</ThemedText>
+            <ThemedText type="default">12/15</ThemedText>
+        </ThemedView>
+
+        <ThemedView style={styles.activityCard}>
+            <ThemedText type="defaultSemiBold">Sleep</ThemedText>
+            <ThemedText type="default">6.80 hours</ThemedText>
+        </ThemedView>
+        
+        </View>
+
+        <View style={styles.progressRow}>
+          <ThemedText type="subtitle" style={styles.sectionTitle}>
+            Progress
+          </ThemedText>
+          <ThemedView style={styles.progressCard}>
+            <ThemedText type="defaultSemiBold">Graf</ThemedText>
+        </ThemedView>
+        </View>
+        
+    </ScrollView>
+    
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingTop: 40,
+  },
+
+  header: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    marginBottom: 5,
+  },
+  topbar_index: {
+    fontSize:22,
+    paddingTop:10,
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+
+  },
+  blue: {
+    color: "#2D7FF9",
+    fontWeight: "600",
+    fontSize: 22,
+  },
+  sectionTitle: {
+    marginVertical: 15,
+  },
+  activityRow: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+  },
+  activityCard: {
+    width: "48%",
+    padding: 15,
+    marginBottom: 12,
+    borderRadius: 12,
+    alignItems: "center",
+    backgroundColor: "#fff",
+    // skygge effekt
+    shadowColor: "#000",
+    shadowOpacity: 0.06,
+    shadowRadius: 8,
+    shadowOffset: { width: 0, height: 4 },
+    elevation: 2,
+  },
+  progressRow: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+  },
+  progressCard: {
+    width: "100%",
+    height: 180,
+    padding: 15,
+    marginBottom: 12,
+    borderRadius: 12,
+    alignItems: "center",
+    backgroundColor: "#fff",
+    // skygge effekt
+    shadowColor: "#000",
+    shadowOpacity: 0.06,
+    shadowRadius: 8,
+    shadowOffset: { width: 0, height: 4 },
+    elevation: 2,
+  },
+});
