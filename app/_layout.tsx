2bd52f03e37888eb430b10818c37785cbd5ae5eb
Snake955 <143405884+Snake955@users.noreply.github.com>
2025-11-26 20:11:56 +0100

    Updated layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
